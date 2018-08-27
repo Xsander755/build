7245f005e47df8a1efac4b4ccd9c7ea17492237d
prima <primakovalex@gmail.com>
2018-08-27 14:55:24 +0300

    1455

---
+++ js/poisk.js
@@ -0,0 +1,58 @@
+ var sech_url;
+ var input = $('.quantity'),
+     timeOut;
+ input.on('keyup', function() {
+
+     clearTimeout(timeOut);
+
+     timeOut = setTimeout(poIsk, 900, $(this).val());
+
+ });
+
+ input.on('keydown', function() {
+
+     clearTimeout(timeOut);
+
+ });
+ input.on('blur', function() {
+     $('.lain_block_glw>div').remove();
+     lOadTabsRey();
+ });
+ input.on('focus', function() {
+     input.value = '';
+ });
+
+ function poIsk(value) {
+
+     sech_url = " https://api.msk-day.ru/puzzle/poi-rating/8a55bf69cf5fb46b8d40f11daca0390a.json?expand=user&name=" + value;
+     sEch_Get(sech_url);
+
+ }
+
+ function sEch_Get(url) {
+     $.ajax({
+         type: 'get',
+         url: url,
+         contentTaype: ' application/json',
+         success: function(data) {
+             $('.lain_block_glw>div').remove();
+             $.each(data, function(i) {
+                 console.log(data[i].user.name);
+                 console.log(data[i].user.picture);
+                 console.log(data[i].seconds);
+                 console.log(data[i].turns);
+                 console.log(data[i]);
+                 $('.lain_block_glw').append('<div class="lb"><div class="icon"><p>' + (i + 1) + '</p></div><div class = "us_phptp" style = "background-image: url(' +
+                     data[i].user.picture + '");></div><div class="us_name"><p>' + data[i].user.name +
+                     '</p></div><div class="time">Время<span>' + data[i].seconds + '</span></div><div class="steps">Шаги<span>' + data[i].turns + '</span></div></div>');
+
+                 $('.lb .time').css('color', '#271c5d');
+                 $('.lb .time span').css('color', '#271c5d');
+                 $('.lb .steps').css('color', '#271c5d');
+                 $('.lb .steps span').css('color', '#271c5d');
+                 $('.lb .us_name p').css('color', '#271c5d');
+             });
+
+         }
+     });
+ }
--- js/reyting.js
+++ js/reyting.js
@@ -1,41 +1,42 @@
 function lOadTabsRey() {
-  var s = 0;
-  $.ajax({
-    type: 'get',
-    url: "https://api.msk-day.ru/puzzle/poi-rating/14391be72342662f13306081dd13c699.json?expand=user",
-    contentTaype: ' application/json',
-    success: function(data) {
-      console.log(data);
-      $.each(data, function(i) {
-        if (i < 20) {
-          console.log(data[i].user.name);
-          console.log(data[i].user.picture);
-          console.log(data[i].seconds);
-          console.log(data[i].turns);
-          $('.lain_block_glw').append('<div class="lb lain' + (i + 1) + '"><div class="icon"><p>' + (i + 1) + '</p></div><div class = "us_phptp" style = "background-image: url(' +
-            data[i].user.picture + '");></div><div class="us_name"><p>' + data[i].user.name +
-            '</p></div><div class="time">Время<span>' + data[i].seconds + '</span></div><div class="steps">Шаги<span>' + data[i].turns + '</span></div></div>');
-          console.log(i);
+    var s = 0;
+    $.ajax({
+        type: 'get',
+        url: "https://api.msk-day.ru/puzzle/poi-rating/14391be72342662f13306081dd13c699.json?expand=user",
+        contentTaype: ' application/json',
+        success: function(data) {
+            console.log(data);
 
-          $(".lain3").nextAll('.time').css('color', 'red');
+            $.each(data, function(i) {
+                if (i < 20) {
+                    console.log(data[i].user.name);
+                    console.log(data[i].user.picture);
+                    console.log(data[i].seconds);
+                    console.log(data[i].turns);
+                    $('.lain_block_glw').append('<div class="lb lain' + (i + 1) + '"><div class="icon"><p>' + (i + 1) + '</p></div><div class = "us_phptp" style = "background-image: url(' +
+                        data[i].user.picture + '");></div><div class="us_name"><p>' + data[i].user.name +
+                        '</p></div><div class="time">Время<span>' + data[i].seconds + '</span></div><div class="steps">Шаги<span>' + data[i].turns + '</span></div></div>');
+                    console.log(i);
 
-          cLeAr();
+                    $(".lain3").nextAll('.time').css('color', 'red');
+
+                    cLeAr();
+                }
+            });
         }
-      });
-    }
-  });
-  console.log("запуск");
+    });
+    console.log("запуск");
 }
 
 
 function cLeAr() {
-  $(".lain1 .icon> *").remove();
-  $(".lain2 .icon> *").remove();
-  $(".lain3 .icon> *").remove();
-  $('.lb .time:gt(2)').css('color', '#271c5d');
-  $('.lb .time:gt(2) span').css('color', '#271c5d');
-  $('.lb .steps:gt(2)').css('color', '#271c5d');
-  $('.lb .steps:gt(2) span').css('color', '#271c5d');
-  $('.lb .us_name:gt(2) p').css('color', '#271c5d');
+    $(".lain1 .icon> *").remove();
+    $(".lain2 .icon> *").remove();
+    $(".lain3 .icon> *").remove();
+    $('.lb .time:gt(2)').css('color', '#271c5d');
+    $('.lb .time:gt(2) span').css('color', '#271c5d');
+    $('.lb .steps:gt(2)').css('color', '#271c5d');
+    $('.lb .steps:gt(2) span').css('color', '#271c5d');
+    $('.lb .us_name:gt(2) p').css('color', '#271c5d');
 
 }
